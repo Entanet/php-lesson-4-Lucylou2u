6c7aeb029c114785d197a74a9925fd0c02bcec70
Lucylou2u <36138313+Lucylou2u@users.noreply.github.com>
2018-04-12 11:17:46 +0100

    Add files via upload

---
+++ app/Books.php
@@ -0,0 +1,53 @@
+ <?php
+
+ class Books
+ {
+    public $booktitle;
+    public $author;
+    public $category;
+    public $releasedate;
+    public $rating;
+    public $comments;  
+
+    public function __construct()
+    {
+    	$this->connection = new PDO('mysql:host=138.68.142.216;dbname=lucy', 'tom', 'hardy');
+    }
+
+    public function show($id)
+    {
+        $stmt = $this->connection->prepare('SELECT * FROM books WHERE id = ?');
+        $stmt->execute([$id]);
+        $stmt->setFetchMode(PDO::FETCH_ASSOC);
+        $result = $stmt->fetchAll()[0];
+
+
+        $this->booktitle = $result['booktitle'];
+        $this->author = $result['author'];
+        $this->category = $result['category'];
+        $this->releasedate = $result['releasedate'];
+        $this->rating = $result['rating']
+        
+
+
+        return $this;
+    }
+
+    public function all()
+    {
+        $stmt = $this->connection->query('SELECT * FROM books'); 
+        $stmt->execute();
+        $stmt->setFetchMode(PDO::FETCH_ASSOC);
+        $rows = $stmt->fetchAll();
+        
+        foreach($rows as $result){
+           $booktitle = new booktitle(); 
+           $booktitle->name = $result['booktitle'];
+           $booktitle->id = $result['id'];
+           $book_library[] = $books; 
+        }
+
+        return $book_library;       
+    }
+ }
+
