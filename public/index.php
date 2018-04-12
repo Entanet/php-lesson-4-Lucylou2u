6e25699ac8082925dee11ec0760ff1b01a83a494
Lucylou2u <36138313+Lucylou2u@users.noreply.github.com>
2018-04-12 10:44:42 +0100

    Add files via upload

---
+++ public/index.php
@@ -0,0 +1,21 @@
+<?php
+require '../app/books.php';
+$p = new books();
+$booktitle= $p->all();
+?>
+
+<html>
+<head>
+	<title>My Book Library</title>
+	<link rel="stylesheet" href="css/skeleton.css" />
+</head>
+
+<body>
+	<table class="u-full-width">
+        <tbody>
+        	<tr>
+        		
+        	</tr>
+
+</body>
+</html>
